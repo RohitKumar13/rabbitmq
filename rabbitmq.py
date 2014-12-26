b8eeb0b099fbf2fc781e0084287ad77bd437d1e6
Sreejith <sreejithc321@gmail.com>
2014-12-26 12:00:48 +0530

    rabbitmq processing

--- rabbitmq.py
+++ rabbitmq.py
@@ -11,6 +11,9 @@ parser.read(path)
 RabbitMQ_hostname = parser.get('RabbitMQ_connection', 'host_name')
 queue_name = parser.get('RabbitMQ_connection', 'queue_name')
 
+def callback(ch, method, properties, body):
+	print " Received %r" % (body,)
+			
 class RabbitMQ(object):
 	
 	def __init__(self):
@@ -24,11 +27,8 @@ class RabbitMQ(object):
 	
 	def receive_data(self, queue_name):
 		self.channel.queue_declare(queue_name)
-		def callback(ch, method, properties, body):
-			print " Received %r" % (body,)
 		self.channel.basic_consume(callback,queue=queue_name,no_ack=True)
 		self.channel.start_consuming()
-
 	
 if __name__ == "__main__":
 	    dRMQ = RabbitMQ()
